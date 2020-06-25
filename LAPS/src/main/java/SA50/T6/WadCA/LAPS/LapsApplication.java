2c7a4eced072ee50d3cdf75b4767450c83d422f4
Jane Lee <e0533359@u.nus.edu>
2020-06-25 18:14:32 +0800

    Inital commit

---
+++ LAPS/src/main/java/SA50/T6/WadCA/LAPS/LapsApplication.java
@@ -0,0 +1,13 @@
+package SA50.T6.WadCA.LAPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapsApplication.class, args);
+	}
+
+}
