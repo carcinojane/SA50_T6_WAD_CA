9cac67e6be0a80f7722a440bf56b7efd27049805
Sherren Soesanto <sherrensoesanto@gmail.com>
2020-06-28 21:51:37 +0800

    Updated staff service and service implementation

--- LAPS/src/main/java/SA50/T6/WadCA/LAPS/controller/StaffController.java
+++ LAPS/src/main/java/SA50/T6/WadCA/LAPS/controller/StaffController.java
@@ -1,5 +1,6 @@
 package SA50.T6.WadCA.LAPS.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import SA50.T6.WadCA.LAPS.model.LeaveRecord;
+import SA50.T6.WadCA.LAPS.model.LeaveRecord.LeaveStatus;
 import SA50.T6.WadCA.LAPS.service.LeaveService;
 import SA50.T6.WadCA.LAPS.service.LeaveServiceImpl;
+import SA50.T6.WadCA.LAPS.service.LeaveTypeImpl;
+import SA50.T6.WadCA.LAPS.service.LeaveTypeService;
+import SA50.T6.WadCA.LAPS.service.StaffService;
+import SA50.T6.WadCA.LAPS.service.StaffServiceImpl;
 
 @Controller
 @RequestMapping("/staff")
@@ -21,6 +27,22 @@ public class StaffController {
 	@Autowired
 	public void setLeaveService(LeaveServiceImpl lserviceImpl) {
 		this.lservice = lserviceImpl;
+	}
+	
+	@Autowired
+	protected LeaveTypeService ltservice;
+	
+	@Autowired
+	public void setLeaveTypeService(LeaveTypeImpl ltserviceImpl) {
+		this.ltservice = ltserviceImpl;
+	}
+	
+	@Autowired
+	protected StaffService sservice;
+	
+	@Autowired
+	public void setStaffService(StaffServiceImpl sserviceImpl) {
+		this.sservice = sserviceImpl;
 	}
 
 	@GetMapping("/login")
@@ -46,25 +68,45 @@ public class StaffController {
 		return ("staff_applyLeave");
 	}
 	
+	@GetMapping("/apply/add")
+	public String applyLeave(@ModelAttribute("LeaveRecord") LeaveRecord leaveRecord, Model model) {
+		leaveRecord = new LeaveRecord();
+		model.addAttribute("leaveTypeList", ltservice.findAllLeaveTypeNames());
+		return "staff_applyLeave_add";
+	}
+	
+	@GetMapping("/apply/save")
+	public String save(@ModelAttribute("LeaveRecord") LeaveRecord leaveRecord) {
+		leaveRecord.setLeaveStatus(LeaveStatus.APPLIED);
+		//set managerID, get based on staffId
+		//set staffID, get based on staffId
+		lservice.saveLeaveRecord(leaveRecord);
+		return "staff_applyLeave";
+	}
+	
 	@GetMapping("/balance")
-	public String balance() {
+	public String balance(Model model, int staffId) {
+		model.addAttribute("staff", sservice.findStaffById(staffId));
 		return "staff_leaveBlance";
 	}
 	
 	@GetMapping("/history")
 	public String history(Model model, int staffId) {
+		//change to session.getAttribute(staffId);
 		model.addAttribute("lrecords", lservice.findLeaveRecordByStaffId(staffId)) ;
 		return "staff_LeaveHistory";
 	}
 	
 	@GetMapping("/history/details")
 	public String leaveDetails(Model model, int id) {
+		//change to session.getAttribute(staffId);
 		model.addAttribute("leave", lservice.findById(id));
 		return "staff_LeaveHistory_details";
 	}
 	
 	@GetMapping("/history/details/edit")
 	public String editLeaveDetails() {
+		//change to session.getAttribute(staffId);
 		return "staff_LeaveHistory_details_edit";
 	}
 	
--- LAPS/src/main/java/SA50/T6/WadCA/LAPS/service/StaffServiceImpl.java
+++ LAPS/src/main/java/SA50/T6/WadCA/LAPS/service/StaffServiceImpl.java
@@ -1,8 +1,12 @@
 package SA50.T6.WadCA.LAPS.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import SA50.T6.WadCA.LAPS.model.Staff;
 import SA50.T6.WadCA.LAPS.repo.StaffRepository;
 
 @Service
@@ -10,6 +14,24 @@ public class StaffServiceImpl implements StaffService {
 
 	@Autowired
 	StaffRepository srepo;
+
+	@Override
+	public Staff findStaffById(int staffId) {
+		return srepo.findById(staffId).get();
+	}
+
+//	@Override
+//	public ArrayList<Float> findLeaveBalanceById(int staffId) {
+//		Staff staff = srepo.findById(staffId).get();
+//		ArrayList<Float> balance = new ArrayList<Float>();
+//		balance.add(staff.getTotalAnnualLeave());
+//		balance.add(staff.getTotalMedicalLeave());
+//		balance.add(staff.getTotalCompensationLeave());
+//		return balance;
+//	}
+	
+
+	
 	
 	
 	
