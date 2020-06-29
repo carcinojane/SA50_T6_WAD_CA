b027efaebe4cb46f5da5ed8a91e6669c627c4ae9
WayneMyo <waiyankm93@gmail.com>
2020-06-29 20:04:35 +0800

    LeaverService > findPendingLeaveRecordByManagerId

--- LAPS/src/main/java/SA50/T6/WadCA/LAPS/service/LeaveServiceImpl.java
+++ LAPS/src/main/java/SA50/T6/WadCA/LAPS/service/LeaveServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import SA50.T6.WadCA.LAPS.model.LeaveRecord;
 import SA50.T6.WadCA.LAPS.repo.LeaveRepository;
